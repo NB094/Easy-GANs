a5e9e845ea5f5fb43bade8f505a855bda7f176be
Niklas Bergen <84533378+NB094@users.noreply.github.com>
2021-09-30 18:17:35 -0600

    Add beta ESRGAN support

--- SRGAN/data_loader.py
+++ SRGAN/data_loader.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imageio
 import cv2
+import tensorflow as tf
 
 class DataLoader():
     def __init__(self, dataset_name, img_res=(128, 128)):
@@ -46,3 +47,49 @@ class DataLoader():
 
     def imread(self, path):
         return imageio.imread(path, pilmode='RGB').astype(np.float)
+
+    
+    def load_data_tf(self, batch_size=1, is_testing=False):
+        data_type = "train" if not is_testing else "test"
+        
+        path = glob('./datasets/%s/*' % (self.dataset_name))
+
+        batch_images = np.random.choice(path, size=batch_size)
+
+        imgs_hr = []
+        imgs_lr = []
+        for img_path in batch_images:
+            #print(f'image path: {img_path}')   # Uncomment this line to display the filename of the image being processed
+            
+            image = tf.read_file(img_path)
+            
+            img_hr = tf.image.decode_jpeg(image, channels=3)
+            img_hr = tf.image.convert_image_dtype(img_hr, dtype=tf.float32)
+            img_hr -= 0.5
+            img_hr /= 0.5
+
+            img_lr = tf.image.decode_jpeg(image, channels=3)
+            img_lr = tf.image.convert_image_dtype(img_lr, dtype=tf.float32)
+            img_lr -= 0.5
+            img_lr /= 0.5
+            
+            h, w = self.img_res
+            low_h, low_w = int(h / 4), int(w / 4)
+
+            img_hr = cv2.resize(image, self.img_res)
+            img_lr = cv2.resize(image, (low_w, low_h))
+
+            # If training => do random flip
+            if not is_testing and np.random.random() < 0.5:
+                img_hr = np.fliplr(img_hr)
+                img_lr = np.fliplr(img_lr)
+
+
+            
+            imgs_hr.append(img_hr)
+            imgs_lr.append(img_lr)
+
+        # imgs_hr = np.array(imgs_hr) / 127.5 - 1.
+        # imgs_lr = np.array(imgs_lr) / 127.5 - 1.
+
+        return imgs_hr, imgs_lr
