53d0f4ea7a22cbd9c4a60f826a489179d39fa141
Niklas Bergen <84533378+NB094@users.noreply.github.com>
2021-10-01 18:12:06 -0600

    Updated Pix2Pix and Streamlit

--- Pix2Pix/Streamlit_Pix2Pix_Generator.py
+++ Pix2Pix/Streamlit_Pix2Pix_Generator.py
@@ -3,26 +3,27 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
 from numpy import expand_dims
 import tensorflow as tf
+import urllib.request
 
 class Generator():
     def __init__(self, img_object, subject):
         self.img_object = img_object        
         self.subject = subject
 
-    def load_image(filename, size=(256,256)):
-        # load image with the preferred size
-        pixels = load_img(filename, target_size=size)
-        # convert to numpy array
-        pixels = img_to_array(pixels)
+    def load_image(self, filename, size=(256,256)):
+        # # load image with the preferred size
+        # pixels = load_img(filename, target_size=size)
+        # # convert to numpy array
+        # pixels = img_to_array(pixels)
+
         # scale from [0,255] to [-1,1]
-        pixels = (pixels - 127.5) / 127.5
+        pixels = (filename - 127.5) / 127.5
         # reshape to 1 sample
         pixels = expand_dims(pixels, 0)
         return pixels
 
-    def generate_image():
-        src_image = load_image(self.img_object)
-        print('Loaded', src_image.shape)
+    def generate_image(self):
+        src_image = self.load_image(self.img_object)
         
         # load model
         if self.subject == 'Human':
@@ -37,6 +38,6 @@ class Generator():
         # scale from [-1,1] to [0,1]
         gen_image = (gen_image + 1) / 2.0
 
-        return gen_image
+        return img_to_array(gen_image)
 
 
--- Pix2Pix/Streamlit_Pix2Pix_Main.py
+++ Pix2Pix/Streamlit_Pix2Pix_Main.py
@@ -3,6 +3,8 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from Streamlit_Pix2Pix_Generator import Generator
+import numpy as np
+import matplotlib.pyplot as plt 
 
 # Page intro
 st.title('Pix2Pix')
@@ -45,13 +47,13 @@ if subject_selection == 'Handbag':
     background_color='#FFFFFF'
 
 
-# bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+bg_image = st.sidebar.file_uploader("Upload trace:", type=["png", "jpg"])
 drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("freedraw", "line", "rect", "circle", "polygon", "transform")
 )
 right_column.text('')
 right_column.text('')
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
+#realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
 
 canvas_result = st_canvas(
@@ -59,10 +61,10 @@ canvas_result = st_canvas(
     stroke_width=1,
     stroke_color=stroke_color,
     background_color=background_color,
-    #background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=310,
-    width=310,
+    background_image=Image.open(bg_image) if bg_image else None,
+    #update_streamlit=realtime_update,
+    height=256,
+    width=256,
     drawing_mode=drawing_mode,
     key="canvas",
 )
@@ -71,11 +73,45 @@ canvas_result = st_canvas(
 
 # Do something interesting with the image data and paths
 
-if canvas_result.image_data is not None:
-    drawn_image = st.image(canvas_result.image_data)
-    gen = Generator(drawn_image, subject)
-    gen_image = gen.generate_image()
-    st.image(gen_image)
+
+# if canvas_result.image_data is not None:
+
+
+
+
+drawn_image = st.image(canvas_result.image_data, output_format = 'JPEG')
+
+drawn_image = canvas_result.image_data
+drawn_image = Image.fromarray(drawn_image, 'RGB')
+drawn_image = np.asarray(drawn_image)
+# drawn_image = np.reshape(drawn_image, (1, 256, 256, 3))
+
+st.text(f'drawn_image array shape: {drawn_image.shape}')
+st.text(f'drawn_image array: {drawn_image}')
+
+
+gen = Generator(drawn_image, subject)
+gen_image = gen.generate_image()
+st.image(gen_image)
+
+
+# st.text(f'gen_image array: {gen_image}')
+
+#st.pyplot(gen_image)
+
+# plt.imshow(gen_image)
+# plt.axis('off')
+# plt.show()
+
+
+
+# right_column.button(label='Generate', on_click=main_Pix2Pix())
+
+# if canvas_result.image_data is not None:
+#     drawn_image = st.image(canvas_result.image_data)
+#     gen = Generator(drawn_image, subject)
+#     gen_image = gen.generate_image()
+#     st.image(gen_image)
 
 # if canvas_result.json_data is not None:
 #     objects = pd.json_normalize(canvas_result.json_data["objects"])
